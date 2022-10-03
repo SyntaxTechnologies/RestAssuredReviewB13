2a14fb7a9c3a318c2bf1b0c850f318c92849f532
moazzamsadiq <moazzam@syntaxtechs.com>
2022-10-03 19:40:09 -0400

    1

---
+++ src/test/java/BasicRestAssured/restAssuredBasic.java
@@ -0,0 +1,15 @@
+package BasicRestAssured;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class restAssuredBasic {
+
+
+
+}
